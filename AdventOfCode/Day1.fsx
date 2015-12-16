460d2677d93626f22edf0e4f6d920069c34c9c90
barld <bakeds@gmail.com>
2015-12-16 11:57:57 +0100

    day1 part 2

--- AdventOfCode/Day1.fsx
+++ AdventOfCode/Day1.fsx
@@ -4,3 +4,9 @@ let input = System.Console.ReadLine()
 let answer = input |> Seq.fold(fun s c -> if c = '(' then s+1 else s-1) 0
 
 printfn "the solution for day one is %i" answer
+
+//day1 part two
+
+let answer2 = (input |> (Seq.mapFold(fun s c -> if c = '(' then s+1,s+1 else s-1 , s-1) 0) |> fst |> Seq.findIndex (fun i -> i = -1)) + 1
+
+printfn "the solution of day1 part2 is %i" answer2
