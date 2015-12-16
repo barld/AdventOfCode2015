d32c13e4d7bddf5486b326f52034e836a93104f2
barld <bakeds@gmail.com>
2015-12-16 20:51:55 +0100

    add 3.2

--- AdventOfCode/Day3.fsx
+++ AdventOfCode/Day3.fsx
@@ -14,4 +14,13 @@ let directions = System.IO.File.ReadAllText(__SOURCE_DIRECTORY__ + "\Day3.txt")
 let coordinatesSet = Set.empty.Add(0,0)
 
 let answer1 = directions |> Seq.fold goToNewcordinate ((0,0), coordinatesSet) |> snd |> Seq.length
+  
+printfn "answer1: %i" answer1
     
+
+let part1 = directions |> Seq.mapi (fun i d -> (i,d)) |> Seq.filter (fun (i,_) -> i%2=0) |> Seq.map snd
+let part2 = directions |> Seq.mapi (fun i d -> (i,d)) |> Seq.filter (fun (i,_) -> i%2=1) |> Seq.map snd
+
+let answer2 = part2 |> Seq.fold goToNewcordinate ((0,0), (part1 |> Seq.fold goToNewcordinate ((0,0), coordinatesSet) |> snd)) |> snd |> Seq.length
+
+printfn "answer2: %i" answer2
