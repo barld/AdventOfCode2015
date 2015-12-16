4780d8c7c1d150ee3229d90e8f7cfbf3dde9e67c
barld <bakeds@gmail.com>
2015-12-16 10:41:28 +0100

    Add day1

---
+++ AdventOfCode/Day1.fsx
@@ -0,0 +1,6 @@
+﻿printfn "input day1:"
+let input = System.Console.ReadLine()
+
+let answer = input |> Seq.fold(fun s c -> if c = '(' then s+1 else s-1) 0
+
+printfn "the solution for day one is %i" answer
