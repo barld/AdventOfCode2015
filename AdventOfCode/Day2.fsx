c8f0b4f78ed427a5041326e4ef69695cd057ede2
barld <bakeds@gmail.com>
2015-12-16 18:55:06 +0100

    add day 2.2

--- AdventOfCode/Day2.fsx
+++ AdventOfCode/Day2.fsx
@@ -11,3 +11,21 @@ let calculateWrapper (present:int*int*int) =
 let presents = System.IO.File.ReadAllLines(__SOURCE_DIRECTORY__ + "\Day2.txt") |> Seq.map parsePresent
 
 let answer = presents |> Seq.map calculateWrapper |> Seq.sum
+
+printfn "answer 1 day2: %i" answer
+
+
+let calculateRibbon (present:int*int*int) =
+    let l, w, h = present
+    (l*w*h) + 
+    (if l > w && l > h then
+        (w+h)*2
+    elif w > h then
+        (h+l)*2
+    else
+        (w+l)*2)
+    
+
+let answer2 = presents |> Seq.map calculateRibbon |> Seq.sum
+
+printfn "answer 2 day2"
